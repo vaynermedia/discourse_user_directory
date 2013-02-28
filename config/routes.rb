0f03a00d1dd1a12bd91e744da58c8bdf725f1558
Sam Bauch <sbauch@gmail.com>
2013-02-27 19:00:19 -0500

    wtf routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do |map|
+Rails.application.routes.draw do 
   get "/directory" => 'directories#index'
 end
 
--- vendor/assets/javascripts/app_route_addition.js
+++ vendor/assets/javascripts/app_route_addition.js
@@ -1,6 +1,15 @@
 /* Add /directory route
 */
 
+// This is our main issue right now. Adding a route to the ember app...
+
+/**
+  Builds the routes for the application
+
+  @method buildRoutes
+  @for Discourse.ApplicationRoute
+**/
+
 (function() {
 
   Discourse.buildRoutes(function() {
