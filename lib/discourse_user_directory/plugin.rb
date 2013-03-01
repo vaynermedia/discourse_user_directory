c4e9f4cc415c8955d697df381cea595513fd1512
Sam Bauch <sbauch@gmail.com>
2013-03-01 12:12:59 -0500

    register css

--- lib/discourse_user_directory/plugin.rb
+++ lib/discourse_user_directory/plugin.rb
@@ -1,4 +1,3 @@
-# lib/discourse_pervasive_banner/plugin.rb
 require File.expand_path('../../../../discourse_plugin/lib/discourse_plugin/discourse_plugin.rb', __FILE__)
 
 module DiscourseUserDirectory
@@ -6,14 +5,13 @@ module DiscourseUserDirectory
   class Plugin < DiscoursePlugin
 
     def setup
-      # Add our Assets
       register_js('directory_controller')
       register_js('directory_route')
       register_js('directory_view')
       register_js('directory.js.handlebars')
       register_js('user')
       register_js('app_route_addition')
-      register_css('user_directory')      
+      register_css('user_directory.css')      
     end
 
   end
