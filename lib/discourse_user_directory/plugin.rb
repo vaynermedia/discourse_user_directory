a0df95e8ecc0fb9cada82988e3632578c07e13c3
Sam Bauch <sbauch@gmail.com>
2013-03-18 16:10:14 -0400

    add translations

--- lib/discourse_user_directory/plugin.rb
+++ lib/discourse_user_directory/plugin.rb
@@ -5,13 +5,14 @@ module DiscourseUserDirectory
   class Plugin < DiscoursePlugin
 
     def setup
-      register_js('directory_controller')
-      register_js('directory_route')
-      register_js('directory_view')
-      register_js('directory.js.handlebars')
-      register_js('user')
-      register_js('app_route_addition')
-      register_css('user_directory.css')      
+            register_css('discourse_user_directory')      
+
+      # register_js('directory_controller')
+      # register_js('directory_route')
+      # register_js('directory_view')
+      # register_js('directory.js.handlebars')
+      # register_js('user')
+      # register_js('app_route_addition')
     end
 
   end
